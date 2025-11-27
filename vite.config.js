8e6f5e23cf401134766bf83de6770e9d599c909c
Moiz-002 <abdulmoizgd@gmail.com>
2025-11-27 22:51:22 +0500

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/"
-})
+  base:
+    process.env.VITE_BASE_PATH ||
+    "/Responsive-React-Product-List-with-Shopping-Cart-Tailwind-CSS-Context-API-",
+});
